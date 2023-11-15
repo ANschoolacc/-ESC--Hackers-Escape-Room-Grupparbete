30da50971572dc3d4386fd35bb2937fd1a2aeb18
Kookiie88 <alexander@jj-media.se>
2023-11-15 19:44:20 +0700

    Created base HTML and JS for filter

---
+++ filterBox.js
@@ -0,0 +1,47 @@
+//Getting DOM elements to be used globally
+const onlineCheckbox = document.querySelector('.filterBox__onlineCheckbox');
+const onSiteCheckbox = document.querySelector('.filterBox__onSiteCheckbox');
+const tagButtons = document.querySelectorAll('.filterBox__tagButton');
+
+//Event listeners for the filter options
+onlineCheckbox.addEventListener('change', filterData);
+onSiteCheckbox.addEventListener('change', filterData);
+tagButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('filterBox__tagButton--selected');
+        filterData();
+    });
+});
+
+//filter function by type (Alex)
+function byType() {
+}
+
+//filter function by tag (Alex)
+function byTag() {
+}
+
+//filter function by rating (Fredrick)
+function byRating() { 
+}
+
+//filter function by keyword search (Fredrick)
+function byKeyword() {
+}
+
+//function that calls all the other filter functions that is run everytime the user clicks on any filter option (Alex)
+function filterData() {
+    let filteredData = arrayn; //need to change this to the array that contains all the cards
+    filteredData = byType(filteredData);
+    filteredData = byRating(filteredData);
+    filteredData = byTag(filteredData);
+    filteredData = byKeyword(filteredData);
+    return filteredData;
+}
+
+//filteredData needs to be used do create the cards that are displayed on the page (Max and Albin)
+
+/*Need to create a function that resets the filter options to default, showing all the cards(Alex and Fredrick)
+(All checkboxes checked, all buttons unselected rating set to 1/5 to 5/5 stars and input field empty)
+This function should be connected to the close button of the filterbox.
+*/
