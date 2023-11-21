108634965d8eeac5d55e7d695797cfa27e1af25b
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2023-11-21 15:27:51 +0100

    Fixed errors caused by conflict

--- fetchChallenges.js
+++ fetchChallenges.js
@@ -1,55 +1,65 @@
-const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+const res = await fetch(
+  "https://lernia-sjj-assignments.vercel.app/api/challenges"
+);
 const data = await res.json();
 export const challenges = data.challenges;
-if(window.location.pathname.includes('ourChallenges')) {
-    const container = document.querySelector('.ourChallenges')
-    renderChallenges(container)
+if (window.location.pathname.includes("ourChallenges")) {
+  const container = document.querySelector(".ourChallenges");
+  renderChallenges(container);
 }
 
 export function renderChallenges(container, threeHighest) {
-    let challengesToRender
-    if(threeHighest) {
-        challengesToRender = challenges.sort((a, b) => b.rating - a.rating).slice(0, 3);
-    } else {
-        challengesToRender = challenges
-    }
-    const challengeTemplate = document.getElementById('card-template')
-    challengesToRender.forEach(challenge => {
-        const template = challengeTemplate.content.cloneNode(true)
-        const card = template.querySelector('article');
-        const image = template.querySelector('.sidescroll__img');
-        const icon = template.querySelector('.sidescroll__icon');
-        const h2 = template.querySelector('h2');
-        const rating = template.querySelector('.sidescroll__rating');
-        const participans = template.querySelector('.sidescroll__participants');
-        const description = template.querySelector('.sidescroll__text');
-        const button = template.querySelector('.sidescroll__btn')
-        button.textContent = challenge.type === 'online' ? 'Take challenge online' : 'Book this room'
-        icon.classList.add(challenge.type === 'online' ? 'fa-laptop' : 'fa-house');
-        card.id = challenge.id
-        image.src = challenge.image // `${challenge.image}?image=${Math.floor(Math.random() * 16)}` to make image random
-        h2.textContent = challenge.title;
-        participans.textContent = challenge.minParticipants === challenge.maxParticipants ? `${challenge.minParticipants} participants` : `${challenge.minParticipants}-${challenge.maxParticipants} participants`
-        description.textContent = challenge.description;
-        createStars(rating.firstElementChild, challenge.rating)
-        container.appendChild(template)
-    })
+  let challengesToRender;
+  if (threeHighest) {
+    challengesToRender = challenges
+      .sort((a, b) => b.rating - a.rating)
+      .slice(0, 3);
+  } else {
+    challengesToRender = challenges;
+  }
+  const challengeTemplate = document.getElementById("card-template");
+  challengesToRender.forEach((challenge) => {
+    const template = challengeTemplate.content.cloneNode(true);
+    const card = template.querySelector("article");
+    const image = template.querySelector(".sidescroll__img");
+    const icon = template.querySelector(".sidescroll__icon");
+    const h2 = template.querySelector("h2");
+    const rating = template.querySelector(".sidescroll__rating");
+    const participans = template.querySelector(".sidescroll__participants");
+    const description = template.querySelector(".sidescroll__text");
+    const button = template.querySelector(".sidescroll__btn");
+    button.textContent =
+      challenge.type === "online" ? "Take challenge online" : "Book this room";
+    icon.classList.add(challenge.type === "online" ? "fa-laptop" : "fa-house");
+    card.id = challenge.id;
+    image.src = challenge.image; // `${challenge.image}?image=${Math.floor(Math.random() * 16)}` to make image random
+    h2.textContent = challenge.title;
+    participans.textContent =
+      challenge.minParticipants === challenge.maxParticipants
+        ? `${challenge.minParticipants} participants`
+        : `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
+    description.textContent = challenge.description;
+    createStars(rating.firstElementChild, challenge.rating);
+    container.appendChild(template);
+  });
 }
 
 function createStars(parent, rating) {
-    for (let i = 0; i < 5; i++) {
-        const iTag = document.createElement('i');
-        iTag.classList.add('fa-star', 'star');
-        if (checkHalfStar(rating, i)) {
-            iTag.classList = 'fa-solid fa-star-half-stroke'
-            parent.appendChild(iTag);
-            continue
-        }
-        i < rating ? iTag.classList.add('fa-solid') : iTag.classList.add('fa-regular')
-        parent.appendChild(iTag);
+  for (let i = 0; i < 5; i++) {
+    const iTag = document.createElement("i");
+    iTag.classList.add("fa-star", "star");
+    if (checkHalfStar(rating, i)) {
+      iTag.classList = "fa-solid fa-star-half-stroke";
+      parent.appendChild(iTag);
+      continue;
     }
+    i < rating
+      ? iTag.classList.add("fa-solid")
+      : iTag.classList.add("fa-regular");
+    parent.appendChild(iTag);
+  }
 }
 
 function checkHalfStar(rating, i) {
-    return (rating.toString().includes('.') && (i + 0.5 === rating))
+  return rating.toString().includes(".") && i + 0.5 === rating;
 }
--- filterBox.js
+++ filterBox.js
@@ -1,55 +1,63 @@
-import { challenges } from './fetchChallenges.js';
+import { challenges } from "./fetchChallenges.js";
 
 //Getting DOM elements to be used globally
-const onlineCheckbox = document.querySelector('.filter__onlineCheckbox');
-const onSiteCheckbox = document.querySelector('.filter__onSiteCheckbox');
-const tagButtons = document.querySelectorAll('.filter__tagButton');
+const onlineCheckbox = document.querySelector(".filter__onlineCheckbox");
+const onSiteCheckbox = document.querySelector(".filter__onSiteCheckbox");
+const tagButtons = document.querySelectorAll(".filter__tagButton");
 
 //Getting search params from URL
 const searchParams = new URL(document.location).searchParams;
-onlineCheckbox.checked = searchParams.get('online') !== 'false';
-onSiteCheckbox.checked = searchParams.get('onsite') !== 'false'; 
+onlineCheckbox.checked = searchParams.get("online") !== "false";
+onSiteCheckbox.checked = searchParams.get("onsite") !== "false";
 
 //Event listeners for the filter options
-onlineCheckbox.addEventListener('change', () => {
-    setSearchParams(`online, ${onlineCheckbox.checked}`)
-    filterData(challenges)
+onlineCheckbox.addEventListener("change", () => {
+  setSearchParams(`online, ${onlineCheckbox.checked}`);
+  filterData(challenges);
 });
-onSiteCheckbox.addEventListener('change', () => {
-    filterData(challenges)
-    setSearchParams(`onsite, ${onSiteCheckbox.checked}`)});
-tagButtons.forEach(button => {
-    searchParams.forEach((value, key) => {
-        if (key === 'tags') {
-            if (value.includes(button.innerHTML.toLowerCase())) {
-                button.classList.add('filter__tagButton--selected');
-            }
-        }
-    })
-    button.addEventListener('click', () => {
-        button.classList.toggle('filter__tagButton--selected');
-        filterData(challenges);
-        setSearchParams(`tags, ${[...document.querySelectorAll('.filter__tagButton--selected')].map(button => button.innerHTML.toLowerCase())}`)
-    });
+onSiteCheckbox.addEventListener("change", () => {
+  filterData(challenges);
+  setSearchParams(`onsite, ${onSiteCheckbox.checked}`);
+});
+tagButtons.forEach((button) => {
+  searchParams.forEach((value, key) => {
+    if (key === "tags") {
+      if (value.includes(button.innerHTML.toLowerCase())) {
+        button.classList.add("filter__tagButton--selected");
+      }
+    }
+  });
+  button.addEventListener("click", () => {
+    button.classList.toggle("filter__tagButton--selected");
+    filterData(challenges);
+    setSearchParams(
+      `tags, ${[
+        ...document.querySelectorAll(".filter__tagButton--selected"),
+      ].map((button) => button.innerHTML.toLowerCase())}`
+    );
+  });
 });
 
 //Filter challenges on page load if search params exist
-if(searchParams.size > 0) {
-    filterData(challenges);
+if (searchParams.size > 0) {
+  filterData(challenges);
 }
 
 function setSearchParams(str) {
-    const searchParams = new URLSearchParams(window.location.search)
-    const [key, value] = str.split(', ')
-    searchParams.set(key, value)
-    const entries = [...searchParams.entries()]
-    entries.forEach(([key, value]) => {
-        if ((!value || value === 'true')) {
-            searchParams.delete(key);
-        }
-    }); 
-    const newRelativePath = searchParams.toString().length > 0 ? window.location.pathname + '?' + searchParams.toString() : window.location.pathname
-    history.pushState(null, '', newRelativePath); 
+  const searchParams = new URLSearchParams(window.location.search);
+  const [key, value] = str.split(", ");
+  searchParams.set(key, value);
+  const entries = [...searchParams.entries()];
+  entries.forEach(([key, value]) => {
+    if (!value || value === "true") {
+      searchParams.delete(key);
+    }
+  });
+  const newRelativePath =
+    searchParams.toString().length > 0
+      ? window.location.pathname + "?" + searchParams.toString()
+      : window.location.pathname;
+  history.pushState(null, "", newRelativePath);
 }
 
 /*
@@ -62,16 +70,20 @@ If neither checkbox is checked, it sets filteredData to an empty array.
 Finally, it returns the filtered list of challenges.
 */
 function byType(filteredData) {
-    if (onlineCheckbox.checked && onSiteCheckbox.checked) {
-        filteredData = challenges;
-    } else if (onlineCheckbox.checked && !onSiteCheckbox.checked) {
-        filteredData = challenges.filter(challenge => challenge.type !== 'onsite');
-    } else if (onSiteCheckbox.checked && !onlineCheckbox.checked) {
-        filteredData = challenges.filter(challenge => challenge.type !== 'online');
-    } else {
-        filteredData = [];
-    }
-    return filteredData;
+  if (onlineCheckbox.checked && onSiteCheckbox.checked) {
+    filteredData = challenges;
+  } else if (onlineCheckbox.checked && !onSiteCheckbox.checked) {
+    filteredData = challenges.filter(
+      (challenge) => challenge.type !== "onsite"
+    );
+  } else if (onSiteCheckbox.checked && !onlineCheckbox.checked) {
+    filteredData = challenges.filter(
+      (challenge) => challenge.type !== "online"
+    );
+  } else {
+    filteredData = [];
+  }
+  return filteredData;
 }
 
 /*
@@ -85,33 +97,35 @@ It then filters the filteredData array to include only challenges whose labels i
 Finally, it returns the newly filtered list of challenges.
 */
 function byTag(filteredData) {
-    const selectedButtons = [];
-    tagButtons.forEach(button => {
-        if (button.classList.contains('filter__tagButton--selected')) {
-        selectedButtons.push(button);
-        }
-    });
-
-    if (selectedButtons.length === 0) {
-        return filteredData;
+  const selectedButtons = [];
+  tagButtons.forEach((button) => {
+    if (button.classList.contains("filter__tagButton--selected")) {
+      selectedButtons.push(button);
     }
+  });
 
-    const selectedTags = selectedButtons.map(button => button.innerHTML.toLowerCase());
-    filteredData = filteredData.filter(challenge => 
-        selectedTags.every(tag => challenge.labels.includes(tag))
-    );
-    
+  if (selectedButtons.length === 0) {
     return filteredData;
+  }
+
+  const selectedTags = selectedButtons.map((button) =>
+    button.innerHTML.toLowerCase()
+  );
+  filteredData = filteredData.filter((challenge) =>
+    selectedTags.every((tag) => challenge.labels.includes(tag))
+  );
+
+  return filteredData;
 }
 
 //filter function by rating (Fredrick)
-function byRating(filteredData) { 
-    return filteredData
+function byRating(filteredData) {
+  return filteredData;
 }
 
 //filter function by keyword search (Fredrick)
 function byKeyword(filteredData) {
-    return filteredData
+  return filteredData;
 }
 
 /*
@@ -123,27 +137,27 @@ Each filter function modifies the filteredData array in place, so the order of t
 Finally, it calls the renderFilteredCards function with the fully filtered data. This function is responsible for updating the UI to display only the challenges that meet all the selected criteria.
 */
 function filterData(challenges) {
-    let filteredData = challenges;
-    filteredData = byType(filteredData);
-    filteredData = byRating(filteredData);
-    filteredData = byTag(filteredData);
-    filteredData = byKeyword(filteredData);
-    renderFilteredCards(filteredData)
+  let filteredData = challenges;
+  filteredData = byType(filteredData);
+  filteredData = byRating(filteredData);
+  filteredData = byTag(filteredData);
+  filteredData = byKeyword(filteredData);
+  renderFilteredCards(filteredData);
 }
 
 function renderFilteredCards(filteredData) {
-    const container = [...document.querySelector('.ourChallenges').children]
-    const zeroChallenges = document.querySelector('.zeroChallenges');
-    const ids = []
-    filteredData.forEach(challenge => {
-        ids.push(challenge.id)
-    })
-    container.forEach(child => {
-        if (!ids.includes(Number(child.id))) {
-            child.style.display = 'none'
-        } else {
-            child.style.display = ''
-        }
-    })
-    zeroChallenges.style.display = filteredData.length === 0 ? 'block' : 'none';
+  const container = [...document.querySelector(".ourChallenges").children];
+  const zeroChallenges = document.querySelector(".zeroChallenges");
+  const ids = [];
+  filteredData.forEach((challenge) => {
+    ids.push(challenge.id);
+  });
+  container.forEach((child) => {
+    if (!ids.includes(Number(child.id))) {
+      child.style.display = "none";
+    } else {
+      child.style.display = "";
+    }
+  });
+  zeroChallenges.style.display = filteredData.length === 0 ? "block" : "none";
 }
