afc55e6a9a0b57a2c27222e516bd51f585df36be
Kookiie88 <alexander@jj-media.se>
2023-11-22 02:17:52 +0700

    Fixed formating so its always 4 spaces for indent

--- booking.js
+++ booking.js
@@ -18,21 +18,21 @@ const bookingContainer = document.querySelector(".booking-container");
 const bookingButtonOne = document.querySelector(".booking-container__button");
 
 const submitBookingButton = document.querySelector(
-  ".booking-container__submit-button"
+    ".booking-container__submit-button"
 );
 
 const challengeTemplate = document.getElementById("card-template");
 
 //Function that fetches data from api
 async function fetchData(url) {
-  const response = await fetch(url);
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error(
-      `Something went wrong with the request. Error code: ${response.status} ${response.statusText}`
-    );
-  }
+    const response = await fetch(url);
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error(
+            `Something went wrong with the request. Error code: ${response.status} ${response.statusText}`
+        );
+    }
 }
 //Declaring empty global variables
 let cardId;
@@ -45,167 +45,169 @@ console.log(challenges);
 /*challenges.forEach((obj) => console.log(obj.id));*/
 
 challenges.forEach((obj) => {
-  const challengeIds = obj.id.toString();
-  console.log(challengeIds);
+    const challengeIds = obj.id.toString();
+    console.log(challengeIds);
 
-  async function fetchDatesApi() {
-    const res = await fetch(
-      `https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=${inputDate}&challenge=${challengeIds}`
-    );
-    const data = await res.json();
-    return data;
+    async function fetchDatesApi() {
+        const res = await fetch(
+            `https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=${inputDate}&challenge=${challengeIds}`
+        );
+        const data = await res.json();
+        return data;
 
-    console.log(fetchDatesApi());
-  }
+        console.log(fetchDatesApi());
+    }
 });
 
 for (let i = 0; i < 30; i++) {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + i);
-  console.log(tomorrow.toISOString().split("T")[0]);
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + i);
+    console.log(tomorrow.toISOString().split("T")[0]);
 }
 
 let bookingBtn = document.querySelectorAll(".sidescroll__btn");
 //for loop that loops through bookingBtn array length
 for (let i = 0; i < bookingBtn.length; i++) {
-  // eventlistener that reacts to booking button on each card
-  bookingBtn[i].addEventListener("click", () => {
-    //If statement that returns function if innertext of button is wrong
-    if (bookingBtn[i].innerText === "Take challenge online") {
-      return;
-    }
-    //sets value of cardId to clicked .sideScroll__btn parent id
-    cardId = bookingBtn[i].parentElement.id;
-    //Changes style of element and adds class
-    bookingContainer.style.display = "block";
-    bookingContainer.classList.add("grow-in");
-    //Changes style of element and removes class
-    document.querySelector(".body").style.overflow = "hidden";
-    document
-      .querySelector(".booking-container__step-one")
-      .classList.remove("invisible");
-    //For loop that creates 30 dates starting from todays date and forward
-    for (let i = 0; i < 30; i++) {
-      const today = new Date();
-      const comingDays = new Date(today);
-      comingDays.setDate(today.getDate() + i);
-      //Adds each date as option in booking-container__date
-      const dateOption = document.createElement("option");
-      dateOption.classList.add("booking-container__date");
-      dateOption.innerText = comingDays.toISOString().split("T")[0];
-      document
-        .querySelector(".booking-container__dateInput")
-        .appendChild(dateOption);
-    }
+    // eventlistener that reacts to booking button on each card
+    bookingBtn[i].addEventListener("click", () => {
+        //If statement that returns function if innertext of button is wrong
+        if (bookingBtn[i].innerText === "Take challenge online") {
+            return;
+        }
+        //sets value of cardId to clicked .sideScroll__btn parent id
+        cardId = bookingBtn[i].parentElement.id;
+        //Changes style of element and adds class
+        bookingContainer.style.display = "block";
+        bookingContainer.classList.add("grow-in");
+        //Changes style of element and removes class
+        document.querySelector(".body").style.overflow = "hidden";
+        document
+            .querySelector(".booking-container__step-one")
+            .classList.remove("invisible");
+        //For loop that creates 30 dates starting from todays date and forward
+        for (let i = 0; i < 30; i++) {
+            const today = new Date();
+            const comingDays = new Date(today);
+            comingDays.setDate(today.getDate() + i);
+            //Adds each date as option in booking-container__date
+            const dateOption = document.createElement("option");
+            dateOption.classList.add("booking-container__date");
+            dateOption.innerText = comingDays.toISOString().split("T")[0];
+            document
+                .querySelector(".booking-container__dateInput")
+                .appendChild(dateOption);
+        }
 
-    //Getting the right challenge and id
-    const challenge = challenges[i];
-    booking.challenge = i;
-    // function for creating participants
-    getMaxAndMinParticipants(challenge);
-    //function for making the titles correct
-    makeTitleForBooking(challenge);
-  });
+        //Getting the right challenge and id
+        const challenge = challenges[i];
+        booking.challenge = i;
+        // function for creating participants
+        getMaxAndMinParticipants(challenge);
+        //function for making the titles correct
+        makeTitleForBooking(challenge);
+    });
 }
 //Eventlistener function that reacts to click on "search available times" button
 bookingButtonOne.addEventListener("click", async () => {
-  //Adds and removes classes of elements
-  document
-    .querySelector(".booking-container__step-one")
-    .classList.add("invisible");
-  document
-    .querySelector(".booking-container__step-two")
-    .classList.remove("invisible");
+    //Adds and removes classes of elements
+    document
+        .querySelector(".booking-container__step-one")
+        .classList.add("invisible");
+    document
+        .querySelector(".booking-container__step-two")
+        .classList.remove("invisible");
 
-  document.querySelector(".second-step").classList.add("fade-in");
-  document
-    .querySelector(".booking-container__submit-button")
-    .classList.add("fade-in");
-  //Sets variable value to the userinput of .booking-container__dateInput
-  let dateInput = document.querySelector(".booking-container__dateInput").value;
-  /*Adds variables into url and runs function fetchData then 
+    document.querySelector(".second-step").classList.add("fade-in");
+    document
+        .querySelector(".booking-container__submit-button")
+        .classList.add("fade-in");
+    //Sets variable value to the userinput of .booking-container__dateInput
+    let dateInput = document.querySelector(
+        ".booking-container__dateInput"
+    ).value;
+    /*Adds variables into url and runs function fetchData then 
 puts return value into dateApi variable*/
-  dateApi = await fetchData(
-    `https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=${dateInput}&challenge=${cardId}`
-  );
+    dateApi = await fetchData(
+        `https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=${dateInput}&challenge=${cardId}`
+    );
 
-  //giving the date key the correct value.
-  booking.date = dateInput;
+    //giving the date key the correct value.
+    booking.date = dateInput;
 
-  console.log(cardId);
-  console.log(dateApi.slots);
-  console.log(dateApi.date);
+    console.log(cardId);
+    console.log(dateApi.slots);
+    console.log(dateApi.date);
 
-  createAvailableTimes(dateApi);
+    createAvailableTimes(dateApi);
 });
 
 submitBookingButton.addEventListener("click", async () => {
-  document
-    .querySelector(".booking-container__step-two")
-    .classList.add("invisible");
-  document
-    .querySelector(".booking-container__step-three")
-    .classList.remove("invisible");
-  document
-    .querySelector(".booking-container__step-three")
-    .classList.add("fade-in");
+    document
+        .querySelector(".booking-container__step-two")
+        .classList.add("invisible");
+    document
+        .querySelector(".booking-container__step-three")
+        .classList.remove("invisible");
+    document
+        .querySelector(".booking-container__step-three")
+        .classList.add("fade-in");
 
-  // getting the values from the different inputs into the object.
+    // getting the values from the different inputs into the object.
 
-  booking.name = nameInput.value;
-  booking.email = emailInput.value;
-  booking.participants = parseInt(particiSelect.value);
-  booking.time = timeInput.value;
+    booking.name = nameInput.value;
+    booking.email = emailInput.value;
+    booking.participants = parseInt(particiSelect.value);
+    booking.time = timeInput.value;
 
-  console.log(booking);
+    console.log(booking);
 
-  const res = await fetch(
-    "https://lernia-sjj-assignments.vercel.app/api/booking/reservations",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(booking),
-    }
-  );
-  const dataBooking = await res.json();
-  console.log(dataBooking);
+    const res = await fetch(
+        "https://lernia-sjj-assignments.vercel.app/api/booking/reservations",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(booking),
+        }
+    );
+    const dataBooking = await res.json();
+    console.log(dataBooking);
 });
 
 //Function to sort through challenges array
 /*challenges.forEach((obj) => console.log(obj.id));
 
-  challenges.forEach((obj) => {
-  const challengeIds = obj.id.toString();
-  console.log(challengeIds);*/
+challenges.forEach((obj) => {
+const challengeIds = obj.id.toString();
+console.log(challengeIds);*/
 
 //making a function that helps me close down the modal and enable scroll on the website once again.
 function escapeBooking() {
-  bookingContainer.style.display = "none";
-  document.querySelector(".body").style.overflow = "scroll";
+    bookingContainer.style.display = "none";
+    document.querySelector(".body").style.overflow = "scroll";
 
-  document
-    .querySelector(".booking-container__step-one")
-    .classList.add("invisible");
-  document
-    .querySelector(".booking-container__step-two")
-    .classList.add("invisible");
-  document
-    .querySelector(".booking-container__step-three")
-    .classList.add("invisible");
+    document
+        .querySelector(".booking-container__step-one")
+        .classList.add("invisible");
+    document
+        .querySelector(".booking-container__step-two")
+        .classList.add("invisible");
+    document
+        .querySelector(".booking-container__step-three")
+        .classList.add("invisible");
 
-  document
-    .querySelectorAll(".booking-container__date")
-    .forEach((element) => element.remove());
+    document
+        .querySelectorAll(".booking-container__date")
+        .forEach((element) => element.remove());
 }
 
 //Making sure you can close the open modal with the esc butto
 document.body.addEventListener("keydown", (e) => {
-  if (e.key == "Escape") {
-    escapeBooking();
-  }
+    if (e.key == "Escape") {
+        escapeBooking();
+    }
 });
 
 //section for later usage
@@ -219,38 +221,38 @@ const booking = {};
 
 //Creating a function that creates option for the participant selector.
 const createParticipants = function (minPar, maxPar) {
-  for (let i = minPar; i <= maxPar; i++) {
-    const participantOption = document.createElement("option");
-    participantOption.classList.add("particiOption");
-    participantOption.value = i;
-    participantOption.innerText = i;
-    particiSelect.appendChild(participantOption);
-    console.log(participantOption.value);
-  }
+    for (let i = minPar; i <= maxPar; i++) {
+        const participantOption = document.createElement("option");
+        participantOption.classList.add("particiOption");
+        participantOption.value = i;
+        participantOption.innerText = i;
+        particiSelect.appendChild(participantOption);
+        console.log(participantOption.value);
+    }
 };
 
 function makeTitleForBooking(obj) {
-  const titleOfChallenge = obj.title;
-  const titleOfBooking = document.querySelector(".booking-container__title");
-  const secondTitle = document.querySelector(".title-two");
-  secondTitle.innerText = `Book room ${titleOfChallenge} (step 2)`;
-  titleOfBooking.innerText = `Book room ${titleOfChallenge} (step 1)`;
+    const titleOfChallenge = obj.title;
+    const titleOfBooking = document.querySelector(".booking-container__title");
+    const secondTitle = document.querySelector(".title-two");
+    secondTitle.innerText = `Book room ${titleOfChallenge} (step 2)`;
+    titleOfBooking.innerText = `Book room ${titleOfChallenge} (step 1)`;
 }
 
 //Function that first gets max and min numbers then also creats options with every number in that span for the select.
 function getMaxAndMinParticipants(obj) {
-  const maxParticipants = obj.maxParticipants;
-  const minParticipants = obj.minParticipants;
+    const maxParticipants = obj.maxParticipants;
+    const minParticipants = obj.minParticipants;
 
-  createParticipants(minParticipants, maxParticipants);
+    createParticipants(minParticipants, maxParticipants);
 }
 
 //function that creats the available times
 function createAvailableTimes(obj) {
-  for (let i = 0; i < obj.slots.length; i++) {
-    const timeSelect = document.querySelector(".time");
-    const timeOption = document.createElement("option");
-    timeOption.innerText = obj.slots[i];
-    timeSelect.appendChild(timeOption);
-  }
+    for (let i = 0; i < obj.slots.length; i++) {
+        const timeSelect = document.querySelector(".time");
+        const timeOption = document.createElement("option");
+        timeOption.innerText = obj.slots[i];
+        timeSelect.appendChild(timeOption);
+    }
 }
